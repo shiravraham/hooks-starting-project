04e57344bd0fb263a2c83636304fe18c63b95c56
Shir <shir.avraham13@gmail.com>
2019-12-07 20:43:10 +0200

    getting ingredients from db

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,13 +7,28 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  // runs after every render cycle
-  // useEffect();
+  // runs after every render cycle, but with an empty array as a second argument it's like componentDidMount
+  useEffect(() => {
+    fetch("https://react-hooks-fc045.firebaseio.com/ingredients.json")
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = ingredient => {
     fetch("https://react-hooks-fc045.firebaseio.com/ingredients.json", {
       method: "POST",
-      body: JSON.stringify({ ingredient }),
+      body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" }
     })
       .then(response => response.json())
