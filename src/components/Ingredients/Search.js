9550ffd73f339a50a194c3c8d75cc73c5272d215
Shir <shir.avraham13@gmail.com>
2019-12-20 20:46:53 +0200

    added timeout for serach input

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,28 +6,35 @@ import "./Search.css";
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
 
-    fetch("https://react-hooks-fc045.firebaseio.com/ingredients.json" + query)
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
+        fetch(
+          "https://react-hooks-fc045.firebaseio.com/ingredients.json" + query
+        )
+          .then(response => response.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+
+            onLoadIngredients(loadedIngredients);
           });
-        }
-
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -37,6 +44,7 @@ const Search = React.memo(props => {
           <input
             type="text"
             value={enteredFilter}
+            ref={inputRef}
             onChange={event => setEnteredFilter(event.target.value)}
           />
         </div>
